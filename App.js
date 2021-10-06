2e9266f707c8aa417241808694cb991482d02e59
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-10-06 16:39:34 -0300

    commit push main 2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Camilo</Text>
+      <Text>Camilo 2</Text>
       <StatusBar style="auto" />
     </View>
   );
